d0e5e760c481f92d8e1d6376ef0a70047f343f55
lmiles77 <80069183+lmiles77@users.noreply.github.com>
2021-06-13 21:59:22 -0500

    added files

---
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -0,0 +1,42 @@
+// add console.log to check to see if code is working
+console.log("working");
+
+// create tile layer to be the background of the map
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// create dark view tile layer as an option for the map
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// add 'graymap' tile layer to the map
+// streets.addTo(map);
+
+// create a base layer that holds both maps
+let baseMaps = {
+    "Streets": streets,
+    "Satellite": satelliteStreets
+};
+
+// create the map object with center and zoom level
+let map = L.map('mapid', {
+        center: [39.5, -98.5],
+        zoom: 3,
+        layers: [streets]
+    });
+    // pass maps layers into the layers control and layers control to the map
+L.control.layers(baseMaps).addTo(map);
+
+
+
+// grabbing GeoJSON data
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+    // creating a GeoJSON layer with the retrieved data
+    L.geoJson(data).addTo(map);
+  });
