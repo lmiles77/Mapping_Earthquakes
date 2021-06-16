a5f57d4258221fbe67d2a021b6c7b7eb7870d16f
lmiles77 <80069183+lmiles77@users.noreply.github.com>
2021-06-16 10:16:38 -0500

    added files

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -39,7 +39,7 @@ L.control.layers(baseMaps).addTo(map);
 // accessing the airport GeoJson URL
 
 
-let torontoData = "https://github.com/lmiles77/Mapping_Earthquakes/blob/main/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
+let torontoData = "https://raw.githubusercontent.com/lmiles77/Mapping_Earthquakes/main/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
 
 // create style for the lines
 let myStyle = {
